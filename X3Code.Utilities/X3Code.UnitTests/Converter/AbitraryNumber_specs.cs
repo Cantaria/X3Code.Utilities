3939278c605e6500f29d50c4cc5bd77cd8cbae4f
Clemens Mattner <cl.mattner@gmail.com>
2020-03-26 18:24:34 +0100

    Converter Added, which converts every number system to decimal

---
+++ X3Code.Utilities/X3Code.UnitTests/Converter/AbitraryNumber_specs.cs
@@ -0,0 +1,30 @@
+﻿using X3Code.Utils.Convert;
+using Xunit;
+
+namespace X3Code.UnitTests
+{
+    public class AbitraryNumber_specs
+    {
+        [Theory]
+        [InlineData("11011010", 2, 218)]
+        [InlineData("DA", 16, 218)]
+        [InlineData("332", 8, 218)]
+        [InlineData("A", 36, 10)]
+        public void ConvertToDecimal(string number, int numberSystemBase, long expected)
+        {
+            var result = AbitraryNumberConverter.ArbitraryToDecimalSystem(number, numberSystemBase);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("11011010", 2, 218)]
+        [InlineData("DA", 16, 218)]
+        [InlineData("332", 8, 218)]
+        [InlineData("A", 36, 10)]
+        public void ConvertFromDecimal(string expected, int numberSystemBase, long number)
+        {
+            var result = AbitraryNumberConverter.DecimalToArbitrarySystem(number, numberSystemBase);
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ X3Code.Utilities/X3Code.Utils/Convert/AbitraryNumberConverter.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace X3Code.Utils.Convert
+{
+    public static class AbitraryNumberConverter
+    {
+        /// <summary>
+        /// Converts the given number from the numeral system with the specified
+        /// radix (in the range [2, 36]) to decimal numeral system.
+        /// <see cref="https://www.pvladov.com/2012/07/arbitrary-to-decimal-numeral-system.html"/>
+        /// </summary>
+        /// <param name="number">The arbitrary numeral system number to convert.</param>
+        /// <param name="radix">The radix of the numeral system the given number is in (in the range [2, 36]).</param>
+        /// <returns></returns>
+        public static long ArbitraryToDecimalSystem(string number, int radix)
+        {
+            const string digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            if (radix < 2 || radix > digits.Length)
+                throw new ArgumentException($"The radix must be >= 2 and <= {digits.Length}");
+
+            if (string.IsNullOrWhiteSpace(number))
+                throw new ArgumentException("number null or empty is not allowed.");
+
+            // Make sure the arbitrary numeral system number is in upper case
+            number = number.ToUpperInvariant();
+
+            long result = 0;
+            long multiplier = 1;
+            for (int i = number.Length - 1; i >= 0; i--)
+            {
+                char c = number[i];
+                if (i == 0 && c == '-')
+                {
+                    // This is the negative sign symbol
+                    result = -result;
+                    break;
+                }
+
+                int digit = digits.IndexOf(c);
+                if (digit == -1)
+                    throw new ArgumentException("Invalid character in the arbitrary numeral system number", "number");
+
+                result += digit * multiplier;
+                multiplier *= radix;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Converts the given decimal number to the numeral system with the
+        /// specified radix (in the range [2, 36]).
+        /// <see cref="https://www.pvladov.com/2012/05/decimal-to-arbitrary-numeral-system.html"/>
+        /// </summary>
+        /// <param name="decimalNumber">The number to convert.</param>
+        /// <param name="radix">The radix of the destination numeral system
+        /// (in the range [2, 36]).</param>
+        /// <returns></returns>
+        public static string DecimalToArbitrarySystem(long decimalNumber, int radix)
+        {
+            const int bitsInLong = 64;
+            const string digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            if (radix < 2 || radix > digits.Length)
+                throw new ArgumentException($"The radix must be >= 2 and <= {digits.Length}");
+
+            if (decimalNumber == 0)
+                return "0";
+
+            int index = bitsInLong - 1;
+            long currentNumber = Math.Abs(decimalNumber);
+            char[] charArray = new char[bitsInLong];
+
+            while (currentNumber != 0)
+            {
+                int remainder = (int)(currentNumber % radix);
+                charArray[index--] = digits[remainder];
+                currentNumber = currentNumber / radix;
+            }
+
+            string result = new String(charArray, index + 1, bitsInLong - index - 1);
+            if (decimalNumber < 0)
+            {
+                result = "-" + result;
+            }
+
+            return result;
+        }
+    }
+}
