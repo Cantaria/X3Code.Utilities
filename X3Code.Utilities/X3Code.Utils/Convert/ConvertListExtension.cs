4b269a060cebd3c995cbc6037a4998ce7985dcc1
Clemens Mattner <cl.mattner@gmail.com>
2020-04-12 19:37:51 +0200

    UTIL-21 Convert Whole List implemented

---
+++ X3Code.Utilities/X3Code.Utils/Convert/ConvertListExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace X3Code.Utils.Convert
+{
+    public static class ConvertListExtension
+    {
+        public static IEnumerable<TDestination> ConvertList<TSource, TDestination>(this IEnumerable<TSource> source, Func<TSource, TDestination> convertFunction)
+        {
+            var result = new List<TDestination>();
+            foreach (var item in source)
+            {
+                var apiModel = convertFunction(item);
+                result.Add(apiModel);
+            }
+            return result;
+        }
+    }
+}
