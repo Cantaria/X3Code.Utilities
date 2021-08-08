facc8bde7e435d932862559c634e268db09d563c
Clemens Mattner <cl.mattner@gmail.com>
2021-08-08 14:13:04 +0200

    DateTime Coverage 100%

--- X3Code.Utilities/X3Code.UnitTests/Extensions/DateTimeExtension_specs.cs
+++ X3Code.Utilities/X3Code.UnitTests/Extensions/DateTimeExtension_specs.cs
@@ -33,9 +33,12 @@ namespace X3Code.UnitTests.Extensions
         public void TheDateShotNotBeLowerThanSqlMin()
         {
             var wrongDate = DateTimeExtension.SqlMinDate.AddDays(-1);
-            var result = wrongDate.ToSqlMinDateTimeIfOutOfRange();
-
-            Assert.Equal(DateTimeExtension.SqlMinDate, result);
+            var wrongResult = wrongDate.ToSqlMinDateTimeIfOutOfRange();
+            Assert.Equal(DateTimeExtension.SqlMinDate, wrongResult);
+            
+            var rightDate = new DateTime(2021,1,1);
+            var result = rightDate.ToSqlMinDateTimeIfOutOfRange();
+            Assert.Equal(rightDate, result);
         }
 
         [Fact]
@@ -55,11 +58,36 @@ namespace X3Code.UnitTests.Extensions
         }
 
         [Fact]
-        public void IfTheNextTuesdayIsNeeded()
+        public void CanGetNextWeekDays()
         {
-            var result = _referenceDate.GetNextTuesday();
+            var monday = _referenceDate.GetNextMonday();
+            var tuesday = _referenceDate.GetNextTuesday();
+            var wednesday = _referenceDate.GetNextWednesday();
+            var thursday = _referenceDate.GetNextThursday();
+            var friday = _referenceDate.GetNextFriday();
+            var saturday = _referenceDate.GetNextSaturday();
+            var sunday = _referenceDate.GetNextSunday();
 
-            Assert.Equal(new DateTime(2010, 11, 16, 12, 43, 0), result);
+            Assert.Equal(new DateTime(2010, 11, 15, 12, 43, 0), monday);
+            Assert.Equal(new DateTime(2010, 11, 16, 12, 43, 0), tuesday);
+            Assert.Equal(new DateTime(2010, 11, 17, 12, 43, 0), wednesday);
+            Assert.Equal(new DateTime(2010, 11, 18, 12, 43, 0), thursday);
+            Assert.Equal(new DateTime(2010, 11, 19, 12, 43, 0), friday);
+            Assert.Equal(new DateTime(2010, 11, 20, 12, 43, 0), saturday);
+            Assert.Equal(new DateTime(2010, 11, 21, 12, 43, 0), sunday);
+        }
+        
+        [Fact]
+        public void CanJumpDays()
+        {
+            var fourteenEarlier = _referenceDate.GetFourteenDaysEarlier();
+            var fourteenLater = _referenceDate.GetFourteenDaysLater();
+            var sevenEarlier = _referenceDate.GetSevenDaysEalier();
+            var sevenLater = _referenceDate.GetSevenDaysLater();
+            Assert.Equal(new DateTime(2010, 11, 01, 12, 43, 0), fourteenEarlier);
+            Assert.Equal(new DateTime(2010, 11, 29, 12, 43, 0), fourteenLater);
+            Assert.Equal(new DateTime(2010, 11, 8, 12, 43, 0), sevenEarlier);
+            Assert.Equal(new DateTime(2010, 11, 22, 12, 43, 0), sevenLater);
         }
     }
 }
