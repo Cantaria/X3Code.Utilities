d4c743bef2349e5e86fff3ae71b333309250ddbc
Clemens Mattner <cl.mattner@gmail.com>
2021-11-13 11:08:18 +0100

    -

--- NuGet/publish.ps1
+++ NuGet/publish.ps1
@@ -1,5 +1,3 @@
-nuget source Add -Name X3Lab -Source "https://gitlab.x3code.de/api/v4/projects/8/packages/nuget/index.json" -UserName Clemens -Password 
-
 ./NuGet.exe push X3Code.Utilities.1.3.1.nupkg -Source X3Lab
 ./NuGet.exe push X3Code.Utilities.1.3.2.nupkg -Source X3Lab
 ./NuGet.exe push X3Code.Utilities.1.3.3.nupkg -Source X3Lab
