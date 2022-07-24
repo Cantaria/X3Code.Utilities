c0cd18ef313dcc407af715a7240a47cfca079205
Clemens Mattner <cl.mattner@gmail.com>
2022-07-24 12:35:48 +0200

    added mass test

--- X3Code.Utilities/X3Code.Infrastructure.Tests/Mockups/PersonMockupFactory.cs
+++ X3Code.Utilities/X3Code.Infrastructure.Tests/Mockups/PersonMockupFactory.cs
@@ -4,17 +4,19 @@ using X3Code.Infrastructure.Tests.Data;
 
 namespace X3Code.Infrastructure.Tests.Mockups
 {
-    public class PersonMockupFactory
+    public static class PersonMockupFactory
     {
-        public static List<Person> GetNPersons(int n)
+        public static List<Person> CreateNPersons(int n)
         {
             var result = new List<Person>();
 
             for (var i = 0; i < n; i++)
             {
+                var month = i % 12 + 1;
+                var day = i % 28 + 1;
                 var person = new Person
                 {
-                    Birthday = new DateTime(1990, i / 12, i / 20),
+                    Birthday = new DateTime(1990, month, day),
                     Name = $"Name-{i}",
                     Surname = $"Surname-{i}"
                 };
--- X3Code.Utilities/X3Code.Infrastructure.Tests/RepositoryTest.cs
+++ X3Code.Utilities/X3Code.Infrastructure.Tests/RepositoryTest.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using X3Code.Infrastructure.Tests.ConstValues;
 using X3Code.Infrastructure.Tests.Data;
 using X3Code.Infrastructure.Tests.Helper;
+using X3Code.Infrastructure.Tests.Mockups;
 using Xunit;
 
 namespace X3Code.Infrastructure.Tests
@@ -46,7 +48,36 @@ namespace X3Code.Infrastructure.Tests
             Assert.Equal(reference.Name, dbPerson.Name);
             Assert.Equal(reference.Surname, dbPerson.Surname);
             Assert.Equal(reference.Birthday, dbPerson.Birthday);
+            Assert.Equal(reference.EntityId, dbPerson.EntityId);
             
+            _database.CleanDataBase();
+        }
+        
+        [Fact]
+        public void MassAddToDatabase()
+        {
+            var reference = PersonMockupFactory.CreateNPersons(10000);
+            var repo = _service.GetRequiredService<IPersonRepository>();
+            
+            repo.AddAll(reference);
+
+            var dataTable = _database.GetFromDb($"{BaseQuery}");
+            Assert.Equal(10000, dataTable.Rows.Count);
+
+            var personsFromDb = dataTable.ToPerson();
+
+            foreach (var referencePerson in reference)
+            {
+                var personFromDb = personsFromDb.SingleOrDefault(x => x.EntityId == referencePerson.EntityId);
+                
+                Assert.NotNull(personFromDb);
+                if (personFromDb == null) return;
+                Assert.Equal(referencePerson.Name, personFromDb.Name);
+                Assert.Equal(referencePerson.Surname, personFromDb.Surname);
+                Assert.Equal(referencePerson.Birthday, personFromDb.Birthday);
+                Assert.Equal(referencePerson.EntityId, personFromDb.EntityId);
+            }
+
             _database.CleanDataBase();
         }
     }
