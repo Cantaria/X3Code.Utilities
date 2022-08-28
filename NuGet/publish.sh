016079d841d578f7da4b4baaa596d49951e5e7e9
Clemens Mattner <cl.mattner@gmail.com>
2022-08-28 12:41:21 +0200

    v1.6.2

--- NuGet/publish.sh
+++ NuGet/publish.sh
@@ -1,6 +1,7 @@
 dotnet nuget push X3Code.Azure.Utils.1.5.0.nupkg --source X3Get
 dotnet nuget push X3Code.Azure.Utils.1.5.6.nupkg --source X3Get
 dotnet nuget push X3Code.Azure.Utils.1.6.0.nupkg --source X3Get
+dotnet nuget push X3Code.Azure.Utils.1.6.2.nupkg --source X3Get
 
 dotnet nuget push X3Code.Utilities.1.3.1.nupkg --source X3Get
 dotnet nuget push X3Code.Utilities.1.3.2.nupkg --source X3Get
@@ -13,6 +14,7 @@ dotnet nuget push X3Code.Utilities.1.5.4.nupkg --source X3Get
 dotnet nuget push X3Code.Utilities.1.5.5.nupkg --source X3Get
 dotnet nuget push X3Code.Utilities.1.5.6.nupkg --source X3Get
 dotnet nuget push X3Code.Utilities.1.6.0.nupkg --source X3Get
+dotnet nuget push X3Code.Utilities.1.6.2.nupkg --source X3Get
 
 dotnet nuget push X3Code.Infrastructure.1.3.1.nupkg --source X3Get
 dotnet nuget push X3Code.Infrastructure.1.3.2.nupkg --source X3Get
@@ -25,6 +27,7 @@ dotnet nuget push X3Code.Infrastructure.1.5.5.nupkg --source X3Get
 dotnet nuget push X3Code.Infrastructure.1.5.6.nupkg --source X3Get
 dotnet nuget push X3Code.Infrastructure.1.6.0.nupkg --source X3Get
 dotnet nuget push X3Code.Infrastructure.1.6.1.nupkg --source X3Get
+dotnet nuget push X3Code.Infrastructure.1.6.2.nupkg --source X3Get
 
 dotnet nuget push X3Code.Infrastructure.RavenDb.1.0.0.nupkg --source X3Get
 dotnet nuget push X3Code.Infrastructure.RavenDb.1.0.2.nupkg --source X3Get
