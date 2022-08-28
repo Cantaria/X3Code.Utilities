1ffe4a8eb701d6ccc0ff95ff616cac9f6dee143b
Clemens Mattner <cl.mattner@gmail.com>
2022-08-28 12:34:13 +0200

    updated NuGet packages

--- NuGet/publish.sh
+++ NuGet/publish.sh
@@ -24,6 +24,7 @@ dotnet nuget push X3Code.Infrastructure.1.5.4.nupkg --source X3Get
 dotnet nuget push X3Code.Infrastructure.1.5.5.nupkg --source X3Get
 dotnet nuget push X3Code.Infrastructure.1.5.6.nupkg --source X3Get
 dotnet nuget push X3Code.Infrastructure.1.6.0.nupkg --source X3Get
+dotnet nuget push X3Code.Infrastructure.1.6.1.nupkg --source X3Get
 
 dotnet nuget push X3Code.Infrastructure.RavenDb.1.0.0.nupkg --source X3Get
 dotnet nuget push X3Code.Infrastructure.RavenDb.1.0.2.nupkg --source X3Get
