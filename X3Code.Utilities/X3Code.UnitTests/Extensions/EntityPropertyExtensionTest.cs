c735e5e579714b776314af5178220ca23252a954
Clemens Mattner <cl.mattner@gmail.com>
2022-12-03 11:56:44 +0100

    #9 Unit-Tests for string-overload

--- X3Code.Utilities/X3Code.UnitTests/Extensions/EntityPropertyExtensionTest.cs
+++ X3Code.Utilities/X3Code.UnitTests/Extensions/EntityPropertyExtensionTest.cs
@@ -1,10 +1,20 @@
 using System;
+using System.Globalization;
+using X3Code.Utils.Extensions;
 using Xunit;
 
 namespace X3Code.UnitTests.Extensions;
 
 public class EntityPropertyExtensionTest
 {
+    public const string StringPropertyName = "StringProperty";
+    public const string IntegerPropertyName = "IntegerProperty";
+    public const string DateTimePropertyName = "DateTimeProperty";
+    public const string DecimalPropertyName = "DecimalProperty";
+    public const string FloatPropertyName = "FloatProperty";
+    public const string DoublePropertyName = "DoubleProperty";
+    public const string BoolPropertyName = "BoolProperty";
+    
     private class PropertyUnitTest
     {
         public string StringProperty { get; set; } = "Unit-Test";
@@ -17,8 +27,33 @@ public class EntityPropertyExtensionTest
     }
     
     [Fact]
-    public void TheDateShotNotBeLowerThanSqlMin()
+    public void CanReadDataAsString()
     {
+        var tester = new PropertyUnitTest();
+        var stringResult = tester.TryReadPropertyAsString(StringPropertyName);
+        var intResult = tester.TryReadPropertyAsString(IntegerPropertyName);
+        var dateTimeResult = tester.TryReadPropertyAsString(DateTimePropertyName);
+        var decimalResult = tester.TryReadPropertyAsString(DecimalPropertyName);
+        var floatResult = tester.TryReadPropertyAsString(FloatPropertyName);
+        var doubleResult = tester.TryReadPropertyAsString(DoublePropertyName);
+        var boolResult = tester.TryReadPropertyAsString(BoolPropertyName);
         
+        Assert.Equal("Unit-Test", stringResult);
+        Assert.Equal("42", intResult);
+        Assert.Equal("2015.05.21 00:00", dateTimeResult);
+        Assert.Equal("42,42", decimalResult);
+        Assert.Equal("24", floatResult);
+        Assert.Equal("12", doubleResult);
+        Assert.Equal("True", boolResult);
+
+        var formatDateResult = tester.TryReadPropertyAsString(DateTimePropertyName, dateTimeFormat: "dd.MM.yyyy");
+        var formatDecimalResult = tester.TryReadPropertyAsString(DecimalPropertyName, "000.00");
+        var formatFloatResult = tester.TryReadPropertyAsString(FloatPropertyName, "000.00");
+        var formatDoubleResult = tester.TryReadPropertyAsString(DoublePropertyName, "000.00");
+        
+        Assert.Equal("21.05.2015", formatDateResult);
+        Assert.Equal("042,42", formatDecimalResult);
+        Assert.Equal("024,00", formatFloatResult);
+        Assert.Equal("012,00", formatDoubleResult);
     }
 }
