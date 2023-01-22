a7b327c54657a356221170210837cea13c08779d
michael <michael@x3-code.de>
2023-01-22 05:07:45 +0100

    Infrastructure.Mobile should work for .NET 6 too ;)

--- X3Code.Utilities/X3Code.Infrastructure.Mobile/IRepository.cs
+++ X3Code.Utilities/X3Code.Infrastructure.Mobile/IRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 
@@ -7,43 +8,45 @@ namespace X3Code.Infrastructure.Mobile
 {
 	public interface IRepository<TEntity> where TEntity : IEntity
 	{
-		TEntity Get(Expression<Func<TEntity, bool>> predicate);
+		TEntity Get(Expression<Func<TEntity, bool>> predicate, bool asNoTracking = false);
 
-		IEnumerable<TEntity> GetAll();
+		IEnumerable<TEntity> GetAll(bool asNoTracking = false);
 
-		IEnumerable<TEntity> GetAll(Expression<Func<TEntity, bool>> predicate);
+		IEnumerable<TEntity> GetAll(Expression<Func<TEntity, bool>> predicate, bool asNoTracking = false);
 
-		void Add(TEntity entity);
+		IQueryable<TEntity> Query(Expression<Func<TEntity, bool>> predicate, bool asNoTracking = false);
 
-		void AddAll(IEnumerable<TEntity> entities);
+		IQueryable<TEntity> Query(bool asNoTracking = false);
 
-		void Remove(TEntity entity);
+		void Add(TEntity entity, bool asNoTracking = false);
 
-		void RemoveAll(IEnumerable<TEntity> entities);
+		void AddAll(IEnumerable<TEntity> entities, bool asNoTracking = false);
 
-		void RemoveRange(Func<TEntity, bool> predicate);
+		void Remove(TEntity entity, bool asNoTracking = false);
 
-		Task RemoveRangeAsync(Func<TEntity, bool> predicate);
+		void RemoveAll(IEnumerable<TEntity> entities, bool asNoTracking = false);
 
-		void Update(TEntity entity);
+		void Update(TEntity entity, bool asNoTracking = false);
 
-		void UpdateAll(IEnumerable<TEntity> entities);
+		void UpdateAll(IEnumerable<TEntity> entities, bool asNoTracking = false);
 
-		Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> predicate);
+		Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> predicate, bool asNoTracking = false);
 
-		Task<IEnumerable<TEntity>> GetAllAsync();
+		Task<IEnumerable<TEntity>> GetAllAsync(bool asNoTracking = false);
+		
+		Task<IEnumerable<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> predicate, bool asNoTracking = false);
 
-		Task<IEnumerable<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> predicate);
+		Task AddAsync(TEntity entity, bool asNoTracking = false);
 
-		Task AddAsync(TEntity entity);
+		Task AddAllAsync(IEnumerable<TEntity> entities, bool asNoTracking = false);
 
-		Task AddAllAsync(IEnumerable<TEntity> entities);
+		Task RemoveAsync(TEntity entity, bool asNoTracking = false);
 
-		Task RemoveAsync(TEntity entity);
+		Task RemoveAllAsync(IEnumerable<TEntity> entities, bool asNoTracking = false);
 
-		Task RemoveAllAsync(IEnumerable<TEntity> entities);
-
-		Task UpdateAsync(TEntity entity);
+		Task UpdateAsync(TEntity entity, bool asNoTracking = false);
+		
+		Task UpdateAllAsync(IEnumerable<TEntity> entities, bool asNoTracking = false);
 	}
 
 }
