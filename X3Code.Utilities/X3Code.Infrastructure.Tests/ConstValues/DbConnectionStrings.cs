21d8ea2ba618a20448119093af2520f44d6ceb0e
Clemens Mattner <cl.mattner@gmail.com>
2023-11-16 13:55:29 +0100

    .Net8

--- X3Code.Utilities/X3Code.Infrastructure.Tests/ConstValues/DbConnectionStrings.cs
+++ X3Code.Utilities/X3Code.Infrastructure.Tests/ConstValues/DbConnectionStrings.cs
@@ -2,7 +2,7 @@
 {
     public static class DbConnectionStrings
     {
-        private const string LocalClemensConnectionString = "Server=virtual.local;Database=UnitTest;User Id=SA;Password=Password1;TrustServerCertificate=True";   //VM Server
+        private const string LocalClemensConnectionString = "Server=localhost;Database=UnitTest;User Id=SA;Password=Password1;TrustServerCertificate=True";   //VM Server
         private const string LocalMichaelConnectionString = "Server=192.168.188.20;Database={0};User Id=sa;Password=yourStrong(!)Password;";   //VM Server
 
         public static string GetConnectionString()
