6b686a52049397ad3fe4457264214bfeacb50be0
Clemens Mattner <cl.mattner@gmail.com>
2024-03-24 14:42:42 +0100

    little improvements for deploy new script

--- NuGet/deploy_Now.sh
+++ NuGet/deploy_Now.sh
@@ -4,7 +4,36 @@ echo "Deplyoing X3-Code NuGet packages"
 echo "Which version are we deploying? Please user format 1.2.3:"
 read version
 
-dotnet nuget push X3Code.Azure.Utils.$version.nupkg --source X3Get
-dotnet nuget push X3Code.Utilities.$version.nupkg --source X3Get
-dotnet nuget push X3Code.Infrastructure.$version.nupkg --source X3Get
-dotnet nuget push X3Code.Wasm.Utils.$version.nupkg --source X3Get
+x3AzureUtils=X3Code.Azure.Utils.$version.nupkg
+x3Utils=X3Code.Utilities.$version.nupkg
+x3Infrastructure=X3Code.Infrastructure.$version.nupkg
+x3Wasm=X3Code.Wasm.Utils.$version.nupkg
+
+echo "Trying to push: $x3AzureUtils"
+if test -f "$x3AzureUtils"; then
+    dotnet nuget push $x3AzureUtils --source X3Get
+else
+    echo "File $x3AzureUtils does not exist. Do nothing"
+fi
+
+echo "Trying to push: $x3Utils"
+if test -f "$x3Utils"; then
+    dotnet nuget push $x3Utils --source X3Get
+else
+    echo "File $x3Utils does not exist. Do nothing"
+fi
+
+echo "Trying to push: $x3Infrastructure"
+if test -f "$x3Infrastructure"; then
+    dotnet nuget push $x3Infrastructure --source X3Get
+else
+    echo "File $x3Infrastructure does not exist. Do nothing"
+fi
+
+echo "Trying to push: $x3Wasm"
+if test -f "$x3Wasm"; then
+    dotnet nuget push $x3Wasm --source X3Get
+else
+    echo "File $x3Wasm does not exist. Do nothing"
+fi
+
