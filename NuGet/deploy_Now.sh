d36a93c1c422131379e7af53796cfdfa12c61d2d
Clemens Mattner <cl.mattner@gmail.com>
2025-01-22 21:01:56 +0100

    changed to new package source, moved package source name to variable

--- NuGet/deploy_Now.sh
+++ NuGet/deploy_Now.sh
@@ -11,31 +11,32 @@ x3AzureUtils=X3Code.Azure.Utils.$version.nupkg
 x3Utils=X3Code.Utilities.$version.nupkg
 x3Infrastructure=X3Code.Infrastructure.$version.nupkg
 x3Wasm=X3Code.Wasm.Utils.$version.nupkg
+PACKAGE_SOURCE=github
 
 echo "Trying to push: $x3AzureUtils"
 if test -f "$x3AzureUtils"; then
-    dotnet nuget push $x3AzureUtils --source X3Get -k $apikey
+    dotnet nuget push $x3AzureUtils --source $PACKAGE_SOURCE -k $apikey
 else
     echo "File $x3AzureUtils does not exist. Do nothing"
 fi
 
 echo "Trying to push: $x3Utils"
 if test -f "$x3Utils"; then
-    dotnet nuget push $x3Utils --source X3Get -k $apikey
+    dotnet nuget push $x3Utils --source $PACKAGE_SOURCE -k $apikey
 else
     echo "File $x3Utils does not exist. Do nothing"
 fi
 
 echo "Trying to push: $x3Infrastructure"
 if test -f "$x3Infrastructure"; then
-    dotnet nuget push $x3Infrastructure --source X3Get -k $apikey
+    dotnet nuget push $x3Infrastructure --source $PACKAGE_SOURCE -k $apikey
 else
     echo "File $x3Infrastructure does not exist. Do nothing"
 fi
 
 echo "Trying to push: $x3Wasm"
 if test -f "$x3Wasm"; then
-    dotnet nuget push $x3Wasm --source X3Get -k $apikey
+    dotnet nuget push $x3Wasm --source $PACKAGE_SOURCE -k $apikey
 else
     echo "File $x3Wasm does not exist. Do nothing"
 fi
